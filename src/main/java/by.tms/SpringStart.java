982cc4adc9a0afeaaed6e1e28685d118a64717fc
Comp <krastynvitaly@gmail.com>
2025-12-03 16:38:55 +0300

    Initial commit

---
+++ src/main/java/by.tms/SpringStart.java
@@ -0,0 +1,13 @@
+package by.tms;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringStart {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Student student = (Student) context.getBean("beanStudent");
+        System.out.println(student.getName());
+    }
+}
