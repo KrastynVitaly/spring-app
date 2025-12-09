05f996961da0a3d698b7cb06e6407d8aef07abf7
Comp <krastynvitaly@gmail.com>
2025-12-09 21:50:42 +0300

    IoC/Di

--- src/main/java/by.tms/SpringStart.java
+++ src/main/java/by.tms/SpringStart.java
@@ -1,13 +1,22 @@
 package by.tms;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@ComponentScan("by.tms")
 public class SpringStart {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-settings.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringStart.class);
 
         Student student = (Student) context.getBean("beanStudent");
         System.out.println(student.getName());
+    }
+
+    @Bean
+    public static Student getStudent() {
+        return new Student();
     }
 }
